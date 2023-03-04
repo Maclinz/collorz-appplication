9cad8b2eae9b664e2287d33c32dff26010365ef5
maclinz <tafadzwamucheke@gmail.com>
2023-03-04 18:14:14 +0000

    colors application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import bg from './bg2.png';
+import bg2 from './grid.png';
+import Palettes from './Components/Palettes'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Palette from "./Components/Palette";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <AppStyled bg={bg} grid={bg2}>
+        <div className="grid"></div>
+        <Routes>
+          <Route path="/" element={<Palettes />} />
+          <Route path="/palette/:id" element={<Palette />} />
+        </Routes>
+      </AppStyled>
+    </BrowserRouter>
   );
 }
+
+
+const AppStyled = styled.div`
+    min-height: 100vh;
+    background-color: slateblue;
+    background-image: url(${props => props.bg});
+    background-repeat: no-repeat;
+    background-size: cover;
+    position: relative;
+    .grid{
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      background-image: url(${props => props.grid});
+      background-repeat: repeat;
+      z-index: 0;
+    }
+`;
 
 export default App;
